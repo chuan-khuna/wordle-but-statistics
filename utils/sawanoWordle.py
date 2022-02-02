48f9d5b558cb7d79b04cce158b66af703677c042
ALTR <chuan.khuna@gmail.com>
2022-02-02 23:09:55 +0700

    added of a sawanese wordle concept

--- utils/sawanoWordle.py
+++ utils/sawanoWordle.py
@@ -75,3 +75,65 @@ class SWordle:
         }
 
         return hint
+
+    def guess(self, guess):
+        status = self.check_word_length(guess)
+
+        if status:
+            score = np.array(self.get_score(self.ans, guess), dtype=np.int)
+            self.current_guess += 1
+        else:
+            score = np.array([0] * len(self.ans), dtype=np.int)
+
+        ans = '-' * len(self.ans)
+        if self.current_guess > self.max_guess or (score == 2).all():
+            ans = self.ans
+
+        guess_result = {
+            'status': status,
+            '#guess': self.current_guess,
+            'max #guess': self.max_guess,
+            'guessed word': guess,
+            'score': score,
+            'answer': ans
+        }
+
+        return guess_result
+
+    def get_score(self, ans, guess):
+        ans = ans.lower()
+        guess = guess.lower()
+
+        # amount of letters in ans
+        ans_letters = {}
+        for letter in ans:
+            if letter in ans_letters.keys():
+                ans_letters[letter] += 1
+            else:
+                ans_letters[letter] = 1
+
+        score = []
+        # check if letters are in the correct position
+        for i, letter in enumerate(guess):
+            if letter == ans[i]:
+                score.append(2)
+                ans_letters[letter] -= 1
+            else:
+                score.append(0)
+
+        # check if letters in guess are not used, score = 1
+        # if it is used exceed the limit, score = 0
+        for i, letter in enumerate(guess):
+            if score[i] == 0 and letter in ans_letters.keys():
+                if ans_letters[letter] - 1 >= 0:
+                    ans_letters[letter] -= 1
+                    score[i] = 1
+
+        return score
+
+    def check_word_length(self, guess):
+        if len(guess) == len(self.ans):
+            return True
+        else:
+            print(f"Guessed word length is not equal to {len(self.ans)}.")
+            return False
